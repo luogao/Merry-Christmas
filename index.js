260afa54ec34bdbcab93b483858433f1a37a1cf6
Roy <gao.luo@honjoinfo.com>
2017-12-21 20:24:44 +0800

    update

--- index.js
+++ index.js
@@ -7,14 +7,21 @@ import './src/css/layout.scss'
 var mySwiper = new Swiper('.swiper-container', {
     speed: 400,
     spaceBetween: 0,
+    allowSlideNext: false,
+    allowSlidePrev: false,
     direction: 'vertical'
 });
 
 $(function() {
-    $('body').on('click', '.image', function() {
-        $('.trigger-button').fadeOut(200, function() {
+    $('body').on('click', '.trigger-button', function() {
+        $(this).fadeOut(200, function() {
             $(this).parents('.image').addClass('open')
         })
+    })
+    $('body').on('click', '.section1 .letter', function() {
+        mySwiper.allowSlideNext = true;
+        mySwiper.slideNext(500)
+        mySwiper.allowSlideNext = false;
     })
     $(document).mouseup(function(e) {
         var _con = $('.image'); // 设置目标区域
