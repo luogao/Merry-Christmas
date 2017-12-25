bea71c562f20b8cab929e3880598438d88d54f42
Roy <gao.luo@honjoinfo.com>
2017-12-25 16:34:20 +0800

    ready to go

--- index.js
+++ index.js
@@ -8,15 +8,101 @@ import './src/css/font.css'
 import './src/css/layout.scss'
 import './src/css/animate.css'
 
-const mySwiper = new Swiper('.swiper-container', {
-    speed: 400,
-    spaceBetween: 0,
-    allowSlideNext: false,
-    allowSlidePrev: false,
-    direction: 'vertical'
-});
+var timer = setInterval(function() {
+    if (document.readyState === "complete") {
+        clearInterval(timer);
+        $('.welcomePage').addClass('loaded')
+        myPromise(() => {
+            $('.welcomePage').remove()
+            $('#app').show()
+            var mySwiper = new Swiper('.swiper-container', {
+                speed: 400,
+                spaceBetween: 0,
+                allowSlideNext: false,
+                allowSlidePrev: false,
+                direction: 'vertical'
+            });
+            $('body')
+                .on('click', '.image', function() {
+                    $(this)
+                        .find('.trigger-button')
+                        .fadeOut(200, function() {
+                            $('.image').addClass('open')
+                        })
+                })
+            $("body").on('click', '[class^="photo-"]', function() {
+                if ($(this).hasClass('zoom')) {
+                    $('[class^="photo-"]').removeClass('zoom')
+                } else {
+                    $('[class^="photo-"]').removeClass('zoom')
+                    $(this).addClass('zoom')
+                }
+            })
+            $('body').on('click', '.section1 .letter', function() {
+                mySwiper.allowSlideNext = true;
+                mySwiper.slideNext(500)
+                myPromise(function() {
+                    $('#dear').show()
+                }, 300).then(myPromise(function() {
+                    document
+                        .getElementById('myAudio')
+                        .play()
+                    if (!$('.flake').length > 0) {
+                        snow.init()
+                    }
+                    dynamicTxtFn().then(() => {
+                        console.log('finish')
+                        $('.flash').fadeOut()
+                        $('#dear').addClass('finish');
+                        myPromise(() => {
+                            $('.letter-stamp').show()
+                        }, 300).then(myPromise(() => {
+                            $('.section2 .img-04').show()
+                        }, 500)).then(myPromise(() => {
+                            $('.section2 .img-04').addClass('swing')
+                        }, 1500))
+                    })
+                }, 900))
+                mySwiper.allowSlideNext = false;
+            })
+            $('body').on('click', '.img-04', function() {
+                mySwiper.allowSlideNext = true;
+                mySwiper.slideNext(500)
+                myPromise(function() {
+                    mySwiper.allowSlideNext = false;
+                    Promise.all(photoshow()).then(() => {
+                        console.log(123)
+                        mySwiper.allowSlideNext = true;
+                        mySwiper.allowSlidePrev = true;
+                        $('.section3 .content-footer').show()
+                    })
+                }, 500)
+            })
 
-let myPromise = function (fn, interval) {
+            $('body').on('click', '.letter-stamp', function() {
+                if ($('.flake').length > 0) {
+                    snow.destroy()
+                } else {
+                    snow.init()
+                }
+            })
+
+            $(document).click(function(e) {
+                var _con = $('.image'); // 设置目标区域
+                if (!_con.is(e.target) && _con.has(e.target).length === 0) {
+                    $('.image').removeClass('open')
+                    setTimeout(() => {
+                        $('.trigger-button').fadeIn(200)
+                    }, 500);
+                }
+            });
+        }, 600)
+    }
+}, 0);
+
+
+
+let myPromise = function(fn, interval) {
     return new Promise((resolve, reject) => {
         let timer = null;
         let context = this;
@@ -29,54 +115,12 @@ let myPromise = function (fn, interval) {
     })
 }
 
-$(function () {
-    $('body')
-        .on('click', '.image', function () {
-            $(this)
-                .find('.trigger-button')
-                .fadeOut(200, function () {
-                    $('.image').addClass('open')
-                })
-        })
-    $('body').on('click', '.section1 .letter', function () {
-        mySwiper.allowSlideNext = true;
-        mySwiper.slideNext(500)
-        myPromise(function () {
-            $('#dear').show()
-        }, 300).then(myPromise(function () {
-            document
-                .getElementById('myAudio')
-                .play()
-            snow()
-            dynamicTxtFn().then(() => {
-                console.log('finish')
-                $('#dear').addClass('finish');
-                myPromise(() => {
-                    $('.letter-stamp').show()
-                }, 300).then(myPromise(() => {
-                    $('.section2 .img-04').show()
-                }, 500)).then(myPromise(() => {
-                    $('.section2 .img-04').addClass('swing')
-                }, 1500))
-            })
-        }, 900))
-        mySwiper.allowSlideNext = false;
+let photoshow = function() {
+    let photoArr = ['.photo-01', '.photo-02', '.photo-03', '.photo-04', '.photo-05', '.photo-06', ]
+    var APromise = photoArr.map((photo, index) => {
+        return myPromise(() => {
+            $(photo).show()
+        }, 1000 * index)
     })
-    $('body').on('click', '.img-04', function () {
-        mySwiper.allowSlideNext = true;
-        mySwiper.slideNext(500)
-        myPromise(function () {
-            mySwiper.allowSlideNext = false;
-        }, 500)
-    })
-    
-    $(document).mouseup(function (e) {
-        var _con = $('.image'); // 设置目标区域
-        if (!_con.is(e.target) && _con.has(e.target).length === 0) {
-            $('.image').removeClass('open')
-            setTimeout(() => {
-                $('.trigger-button').fadeIn(200)
-            }, 500);
-        }
-    });
-})
+    return APromise
+}
--- src/js/dynamicTxt.js
+++ src/js/dynamicTxt.js
@@ -2,19 +2,16 @@ const dynamicTxtFn = function() {
     return new Promise((resolve, reject) => {
         let timeout = 40
         let element = document.getElementById('dynamicTxt').getElementsByClassName('txt')[0];
-        let testString = `请注意：以上两句声明中的宋体和微软雅黑不应该调换（尽管调换了也不会发生错误），这是因为从字体的式样来看，微软雅黑是非衬线的，而宋体才是衬线的。然而中文并不像英文那样严格区分字体族，所以这一点在实际应用当中并不那么重要。
-
-        别忘了照顾不同的操作系统
-        作为一个 Web 开发者，你理应对 Windows, Mac OS, Linux 家族等常用操作系统里的系统字体有足够的了解，特别是中文。在这里，我们假设目标网站要同时给予 windows 用户和 mac 用户最好的字体体验，于是我们可以这样声明：
-        
-        Font-family: Helvetica, Tahoma, Arial, STXihei, “华文细黑”, “Microsoft YaHei”, “微软雅黑”, sans-serif
-        
-        这句声明都做到哪些事情呢？让我们一一说明（括号内代表其对应的目标操作系统）：
-        ;`
+        let fullString = `老婆，圣诞节快乐！  
+        <br/>
+        <span style="padding-left:.75rem;"/>转眼间，一年即将结束。回想历历在目，从你到上海那天起，我们的生活发生了翻天覆地的变化。这一年来，很感恩老婆你对我的照顾。因为有你在我身边督促我，鼓励我，安慰我，鞭策我。我过的很幸福。在我眼里酷酷的你，内心却住着一个小仙女🧚。我逐渐熟悉了你的喜怒哀乐。更重要的是我习惯了有你的生活。日子无非柴米油盐酱醋茶，但却因你大不相同。感恩这日子里所有的事都有你在我身旁。愿往后彼此亲密无间，平淡生活仍能甜甜蜜蜜。
+        <br/>
+        <span style="padding-left:.75rem;"/>圣诞节快乐，爱你!
+        `
         let showString = '';
         let showTxtFn = () => {
-            if (showString.length < testString.length) {
-                showString = testString.substring(0, showString.length + 1)
+            if (showString.length < fullString.length) {
+                showString = fullString.substring(0, showString.length + 1)
                 element.innerHTML = showString
                 setTimeout(showTxtFn, timeout);
             } else {
